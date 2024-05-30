15b884facc07dc4d747a5e3b66b9d87ab472d76b
raicon480 <133657180+raicon480@users.noreply.github.com>
2024-05-29 21:19:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-const botoes = document.querySelectorALL{".botao"};
+const botoes = document.querySelectorALL(".botao");
 
+for(let i=0;i ,botoes.length;i++){
+    botoes[i].onclick = function(){
+    for(let j=0; j<botoes.length;j++){
+        botoes[j].classList.remove("ativo");
+    }
+    botoes[i].classList.add("ativo")
+    }
+    console.log();
+}
